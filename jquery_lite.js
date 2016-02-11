47fe26402c5a752917979dd649387d186c4da7dc
Ryan Chong <chongr@umich.edu>
2016-02-11 15:28:16 -0800

    complete jQuery lite

--- jquery_lite.js
+++ jquery_lite.js
@@ -2,6 +2,8 @@
   if (typeof jquery === "undefined") {
     window.jquery = {};
   }
+  document.addEventListener("DOMContentLoaded", runCallbacks);
+  var callbackArray = [];
 
   function DOMNodeCollection (array) {
     this.array = array;
@@ -110,16 +112,85 @@ DOMNodeCollection.prototype.remove = function () {
   this.array = [];
 };
 
+DOMNodeCollection.prototype.on = function(type, callback) {
+  this.array.forEach( function (el) {
+    el.addEventListener(type, callback);
+  });
+};
+
+DOMNodeCollection.prototype.off = function(type, callback) {
+  this.array.forEach( function (el) {
+    el.removeEventListener(type, callback);
+  });
+};
+
 function $l(selector) {
   if (typeof selector === "string") {
     var nodeList = document.querySelectorAll(selector);
     var elementArray = [].slice.call(nodeList);
   } else if (typeof selector === "object" && selector instanceof HTMLElement) {
     var elementArray = [selector];
+  } else if (typeof selector === "function") {
+    callbackArray.push(selector);
+    if (document.readyState === "complete") {
+      selector();
+    }
   }
-
-  return new DOMNodeCollection(elementArray);
+  if (elementArray) {
+    return new DOMNodeCollection(elementArray);
+  }
 }
 
-  window.jquery.$l = $l;
+var extend = function () {
+  var args = [].slice.apply(arguments);
+  var returnObject = args.shift();
+
+  args.forEach(function (el) {
+    var currentElKeys = Object.keys(el);
+    currentElKeys.forEach( function (key) {
+      returnObject[key] = el[key];
+    });
+  });
+
+  return returnObject;
+};
+
+var ajax = function (options) {
+  var defaults = {
+    type: 'GET',
+    url: window.location.href,
+    data: {},
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    success: function (data) {alert("success"); console.log(data);},
+    error: function () {alert("error");}
+  };
+
+  var newOptions = extend(defaults, options);
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+      if (xmlhttp.status === 200) {
+        var data = JSON.parse(xmlhttp.responseText);
+        newOptions.success(data);
+      } else {
+      newOptions.error();
+      }
+    }
+  };
+
+  xmlhttp.open(newOptions['type'], newOptions['url']);
+  xmlhttp.send();
+
+};
+
+function runCallbacks() {
+  callbackArray.forEach( function(el) {
+    el();
+  });
+  callbackArray = [];
+}
+
+  window.$l = $l;
+  window.$l.extend = extend;
+  window.$l.ajax = ajax;
 })();
